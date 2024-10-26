c7e8e7b036a20551de65845eddd4b1c475250f2d
Christopher <chrismartinez.2054@gmail.com>
2024-10-26 00:45:25 -0600

    added components

---
+++ src/app/kanban-task/kanban-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './kanban-task.component.html',
+  styleUrl: './kanban-task.component.css'
+})
+export class KanbanTaskComponent {
+
+}
